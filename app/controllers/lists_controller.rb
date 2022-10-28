2af54b13817d6009f25af906987bac474cf0dbab
Liam Strickland <liam.strickland96@gmail.com>
2022-10-28 16:16:32 +0200

    added delete button

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,12 @@ class ListsController < ApplicationController
     redirect_to list_path(@list)
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
